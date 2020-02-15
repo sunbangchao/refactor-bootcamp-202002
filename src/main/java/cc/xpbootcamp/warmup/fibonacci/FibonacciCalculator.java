3641f0cec4a40eabda93bfa790a0d6c5cd4959ee
SunBangchao <>
2020-02-15 17:06:31 +0800

    when position is 1 or 2

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
@@ -1,10 +1,23 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class FibonacciCalculator {
 
+    private static List<Long> list = initList();
 
     public static long calculateByPosition(int position){
+        if(list.size() >= position) return list.get(position-1);
+
         return 0;
+    }
+
+    private static List<Long> initList(){
+        List<Long> list = new LinkedList<>();
+        list.add(1L);
+        list.add(1L);
+        return list;
     }
 
 }

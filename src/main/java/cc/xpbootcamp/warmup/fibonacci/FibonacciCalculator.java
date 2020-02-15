d76a90621364e940a51ee74161c38bd26998a472
SunBangchao <>
2020-02-15 17:10:50 +0800

    when position > 2

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
@@ -8,9 +8,10 @@ public class FibonacciCalculator {
     private static List<Long> list = initList();
 
     public static long calculateByPosition(int position){
-        if(list.size() >= position) return list.get(position-1);
-
-        return 0;
+        for (int i = list.size(); i < position; i++) {
+            list.add(list.get(i-1) + list.get(i-2));
+        }
+        return list.get(position-1);
     }
 
     private static List<Long> initList(){

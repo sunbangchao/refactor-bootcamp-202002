daa26b8adfb1c9571ca7fe5500bc1ab33f675105
SunBangchao <>
2020-02-19 14:58:12 +0800

    更新测试用例，补充时间

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -2,6 +2,8 @@ package cc.xpbootcamp.warmup.cashier;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -11,35 +13,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 class OrderReceiptTest {
-//    @Test
-//    void shouldPrintCustomerInformationOnOrder() {
-//        Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<LineItem>());
-//        OrderReceipt receipt = new OrderReceipt(order);
-//
-//        String output = receipt.printReceipt();
-//
-//
-//        assertThat(output, containsString("Mr X"));
-//        assertThat(output, containsString("Chicago, 60601"));
-//    }
 
-//    @Test
-//    public void shouldPrintLineItemAndSalesTaxInformation() {
-//        List<LineItem> lineItems = new ArrayList<LineItem>() {{
-//            add(new LineItem("milk", 10.0, 2));
-//            add(new LineItem("biscuits", 5.0, 5));
-//            add(new LineItem("chocolate", 20.0, 1));
-//        }};
-//        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
-//
-//        String output = receipt.printReceipt();
-//
-//        assertThat(output, containsString("milk\t10.0\t2\t20.0\n"));
-//        assertThat(output, containsString("biscuits\t5.0\t5\t25.0\n"));
-//        assertThat(output, containsString("chocolate\t20.0\t1\t20.0\n"));
-//        assertThat(output, containsString("Sales Tax\t6.5"));
-//        assertThat(output, containsString("Total Amount\t71.5"));
-//    }
 
     @Test
     public void should_print_line_item_and_sales_tax_and_total_amount_information(){
@@ -54,6 +28,8 @@ class OrderReceiptTest {
         OrderReceipt receipt = new OrderReceipt(new Order(calendar.getTime(), lineItems));
         String output = receipt.printReceipt();
 
+        DateFormat format = new SimpleDateFormat("yyyy年MM月dd日，EEEE");
+        assertThat(output,containsString(format.format(calendar.getTime())));
         assertThat(output,containsString("milk, 10.00 × 2, 20.00"));
         assertThat(output,containsString("biscuits, 5.00 × 5, 25.00"));
         assertThat(output,containsString("chocolate, 20.00 × 1, 20.00"));
@@ -74,6 +50,8 @@ class OrderReceiptTest {
         OrderReceipt receipt = new OrderReceipt(new Order(calendar.getTime(), lineItems));
         String output = receipt.printReceipt();
 
+        DateFormat format = new SimpleDateFormat("yyyy年MM月dd日，EEEE");
+        assertThat(output,containsString(format.format(calendar.getTime())));
         assertThat(output,containsString("milk, 10.00 × 2, 20.00"));
         assertThat(output,containsString("biscuits, 5.00 × 5, 25.00"));
         assertThat(output,containsString("chocolate, 20.00 × 1, 20.00"));
